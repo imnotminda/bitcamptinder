6799a8c92833bf223500f66255ab9262774e92ac
hyejinbao <srin5@naver.com>
2024-10-17 19:42:26 +0900

    회원정보 js, jsp 수정완료 불러오기 디폴트값 확인

--- src/main/webapp/WEB-INF/js/update.js
+++ src/main/webapp/WEB-INF/js/update.js
@@ -9,18 +9,7 @@ $(function() {
             $.ajax({
                 type: 'post',
                 url: '/BitcampTinder/user/update',
-                data: {
-                    user_username: $('#user_username').val(), // 사용자 이름 추가
-                    user_pwd: $('#user_pwd').val(), // 비밀번호만 전송
-                    user_mbti: $('#user_mbti').val(), // MBTI
-                    user_religion: $('#user_religion').val(), // 종교
-                    user_tel1: $('#user_tel1').val(), // 전화번호 1
-                    user_tel2: $('#user_tel2').val(), // 전화번호 2
-                    user_tel3: $('#user_tel3').val(), // 전화번호 3
-                    user_email1: $('#user_email1').val(), // 이메일 앞부분
-                    user_email2: $('#user_email2').val(), // 이메일 도메인
-                    user_email3: $('#user_email3').val() // 이메일 선택
-                },
+                data: $('#updateForm').serialize(), // serialize() 메서드를 직접 사용
                 success: function() {
                     alert('회원정보 수정 완료');
                     location.href = '/BitcampTinder';
